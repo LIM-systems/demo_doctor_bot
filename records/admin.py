63b1ee470f41939764ad2179ccdd2b68e087917b
a.zemtsov <elchako@ya.ru>
2022-11-23 14:50:06 +0300

    + users

--- records/admin.py
+++ records/admin.py
@@ -1,26 +1,30 @@
 from django.contrib import admin
 
-from records.models import Clients, Doctors, Records, Specials
+from records.models import Clients, Doctors, Records, Specials, Users
 
 
+@admin.register(Doctors)
 class DoctorsAdmin(admin.ModelAdmin):
     list_display = ('name', 'achiv_short', 'email', 'special')
     search_fields = ('name', 'special')
     list_filter = ('name', 'special')
 
 
+@admin.register(Specials)
 class SpecialsAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
     list_filter = ('name',)
 
 
+@admin.register(Clients)
 class ClientsAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone')
     search_fields = ('name', 'phone')
     list_filter = ('name',)
 
 
+@admin.register(Records)
 class RecordsAdmin(admin.ModelAdmin):
     list_display = ('user', 'doctor', 'date', 'time', 'finish')
     search_fields = ('user', 'doctor', 'date', 'time', 'finish')
@@ -28,7 +32,6 @@ class RecordsAdmin(admin.ModelAdmin):
     ordering = ('date', 'time')
 
 
-admin.site.register(Doctors, DoctorsAdmin)
-admin.site.register(Specials, SpecialsAdmin)
-admin.site.register(Records, RecordsAdmin)
-admin.site.register(Clients, ClientsAdmin)
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name',)
